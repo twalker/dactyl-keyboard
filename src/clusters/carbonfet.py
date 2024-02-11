28915b899d30b96861959122d8857170acb830b6
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2024-02-11 10:34:07 -0500

    adjusted screw on carbonfet cluster

--- src/clusters/carbonfet.py
+++ src/clusters/carbonfet.py
@@ -400,7 +400,7 @@ class CarbonfetCluster(DefaultCluster):
 
     def screw_positions(self):
         position = self.thumborigin()
-        position = list(np.array(position) + np.array([-48, -37, 0]))
+        position = list(np.array(position) + np.array([-54, -37, 0]))
         position[2] = 0
 
         return position
