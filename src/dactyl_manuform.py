180afd734ee8ecec2cb91dffa5afb2f62f89514a
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2024-04-04 18:34:01 -0400

    Fixes for nice!view

--- src/dactyl_manuform.py
+++ src/dactyl_manuform.py
@@ -2038,20 +2038,24 @@ def make_dactyl():
 
         return shape
 
+    def is_cq():
+        return ENGINE == 'cadquery'
 
     def oled_undercut_mount_frame(side='right'):
         mount_ext_width = oled_mount_width + 2 * oled_mount_rim
-        mount_ext_height = oled_mount_height + 2 * oled_mount_rim
+        mount_ext_height = oled_mount_height + 2 * oled_mount_undercut_height_rim
         hole = box(mount_ext_width, mount_ext_height, oled_mount_cut_depth + .01)
 
         shape = box(mount_ext_width, mount_ext_height, oled_mount_depth)
         shape = difference(shape, [box(oled_mount_width, oled_mount_height, oled_mount_depth + .1)])
         undercut = box(
             oled_mount_width + 2 * oled_mount_undercut,
-            oled_mount_height + 2 * oled_mount_undercut,
+            oled_mount_height + 2 * oled_mount_undercut_height,
             oled_mount_depth)
         undercut = translate(undercut, (0., 0., -oled_mount_undercut_thickness))
         shape = difference(shape, [undercut])
+        if is_cq():
+            shape = shape.edges(">Z").chamfer(0.7)
 
         oled_mount_location_xyz, oled_mount_rotation_xyz = oled_position_rotation(side=side)
 
